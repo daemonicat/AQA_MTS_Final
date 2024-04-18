95d8406344ccaa973b28d2df7946d21930ba8f14
Daemonicat <dima62131@gmail.com>
2024-04-18 18:54:35 +0300

    Fixed create test

--- QaseTestProject/Objects/Steps/CreateNewProjectSteps.cs
+++ QaseTestProject/Objects/Steps/CreateNewProjectSteps.cs
@@ -17,19 +17,12 @@ public class CreateNewProjectSteps : BaseSteps
     public ProjectPage CreateProject(string name, string code, string description, string projectAccessType, string? memberAccessType)
     {
         _projectsPage.ClickCreateNewProjectButton();
-        Thread.Sleep(2000);
         _createProjectDialogue.FillInNameField(name);
-        Thread.Sleep(2000);
         _createProjectDialogue.FillInProjectCode(code);
-        Thread.Sleep(2000);
         _createProjectDialogue.FillInProjectDescription(description);
-        Thread.Sleep(2000);
         _createProjectDialogue.SetProjectAccessType(projectAccessType);
-        Thread.Sleep(2000);
         if (projectAccessType != "public") {_createProjectDialogue.SetMemberAccessType(memberAccessType!);}
-        Thread.Sleep(2000);
         _createProjectDialogue.ClickCreateProjectButton();
-        Thread.Sleep(2000);
         
         return new ProjectPage(Driver);
     }
